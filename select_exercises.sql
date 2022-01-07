901226e336ee661e66bd0c32d379612aa3f60f62
Nathan Sharick <sharick.nathan@gmail.com>
2022-01-07 10:33:56 -0600

    exercise update

--- select_exercises.sql
+++ select_exercises.sql
@@ -14,7 +14,7 @@ SELECT name, release_date FROM `albums` ORDER BY release_date;
 SELECT artist, name FROM albums WHERE artist = 'Pink Floyd';
 SELECT name, release_date FROM albums WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
 SELECT name, genre FROM albums WHERE name = 'Nevermind'; 
-SELECT name, release_date FROM albums WHERE release_date >= '1990';
+SELECT name, release_date FROM albums WHERE release_date BETWEEN '1990' AND '1999';
 SELECT name, sales FROM albums WHERE sales < 20;
 SELECT name, genre FROM albums WHERE genre = 'Rock'; 
 
