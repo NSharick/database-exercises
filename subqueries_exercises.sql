82ce5ff2cf1a519b1c11bc80cdb83cfe0b6b7437
Nathan Sharick <sharick.nathan@gmail.com>
2022-01-12 16:14:45 -0600

    subqueries exercise updates

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -11,24 +11,61 @@ WHERE hire_date =
 #this query returned 55 current employees that were hired on the same day as employee number 101010.
 
 #2
-SELECT e.first_name, t.title 
+SELECT t.title 
 FROM employees AS e
 JOIN titles AS t
 ON e.emp_no = t.emp_no
-WHERE e.first_name = 'Aamod' AND t.to_date = '9999-01-01';
+WHERE e.first_name = 'Aamod' AND t.to_date = '9999-01-01'
+GROUP BY t.title
+ORDER BY t.title;
 
-#This query only gives the current title for current employees names Aamod.....
-
+SELECT DISTINCT title
+FROM titles
+WHERE emp_no IN (SELECT emp_no FROM employees JOIN dept_emp USING(emp_no) WHERE first_name = 'aamod' AND to_date > NOW()); 
+#first set with only JOIN and second set with subquery both seem to give the same answers.
 
 #3
+#used this to find all the employees who are currently working at the company. (240124) 
+SELECT emp_no FROM dept_emp WHERE to_date > NOW();
 
+#used this to get a cound of unique employee numbers as listed in the employees table. (300024)
+SELECT COUNT(emp_no) FROM employees;
+
+#combine the two previous to count only employee numbers that are not included in the group of current employees.
+SELECT COUNT(emp_no) FROM employees
+WHERE emp_no NOT IN (SELECT emp_no FROM dept_emp WHERE to_date > NOW());
+#returns 59900 employees who no longer work at the company
 
 #4
+#used this to pull the employee numbers for the current department managers
+SELECT emp_no FROM dept_manager WHERE to_date > NOW();
 
+#this returns a table with employee number fist and last name, and gender for all the employees who were in the previous query results (current managers) 
+SELECT emp_no, first_name, last_name, gender
+FROM employees
+WHERE emp_no IN (SELECT emp_no FROM dept_manager WHERE to_date > NOW());
+
+#this returns results with only female employees from the previous list and answers the exercise question. 
+SELECT emp_no, first_name, last_name, gender
+FROM employees
+WHERE emp_no IN (SELECT emp_no FROM dept_manager WHERE to_date > NOW()) AND gender = 'F';
 
 #5
+#returns all the current salaries
+SELECT salary FROM salaries WHERE to_date > NOW();
 
+#returns the historical average salary
+SELECT AVG(salary) FROM salaries;
 
+#puts the previous two queries together to return a list of all the salaries that are greater than the company historical average but only includes a column for the salary. 
+SELECT salary FROM salaries WHERE to_date > NOW() AND salary > (SELECT AVG(salary) FROM salaries);
+
+#joined the employees table and the salaries table to make the employee name availabe and then added the sub query to get the final results for the exercise.
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS employee_name, s.salary
+FROM employees AS e
+JOIN salaries AS s 
+ON s.emp_no = e.emp_no 
+WHERE to_date > NOW() AND salary > (SELECT AVG(salary) FROM salaries);
 
 #6
 ##########
@@ -52,7 +89,7 @@ SELECT MAX(salary) - STDDEV(salary) FROM salaries WHERE to_date > NOW();
 #used this to find the count of current salaries that are within 1 standard deviation from the max salary (83)
 SELECT COUNT(salary) AS sal_in_1
 FROM salaries
-WHERE salary >= (SELECT MAX(salary) - STDDEV(salary) FROM salaries WHERE to_date > NOW()) AND to_date > NOW();
+WHERE salary >= (SELECT MAX(salary) - STDDEV(salary) FROM salaries WHERE to_date > NOW()); AND to_date > NOW();
 
 #then I put it all together in the same way as the z-score example in the book by taking the code for the count of current salaries within 1 standard deviation of the max in () and dividing it by the code for the total count of current salaries in () and then multiplying that by 100.
 SELECT
@@ -62,5 +99,6 @@ WHERE salary >= (SELECT MAX(salary) - STDDEV(salary) FROM salaries WHERE to_date
 /
 (SELECT COUNT(salary) AS num_sal
 FROM salaries
-WHERE to_date > NOW()) * 100 AS percent_of_salaries;
+WHERE to_date > NOW()) 
+* 100 AS percent_of_salaries;
 
