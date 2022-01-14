45f57d329224d1749cf654b7210da4ba766698d6
Nathan Sharick <sharick.nathan@gmail.com>
2022-01-14 16:59:07 -0600

    exercise updates

--- more_exercises.sql
+++ more_exercises.sql
@@ -1,4 +1,71 @@
 #MORE EXERCISES#
+#Extra MySQL Exercises#
+
+#More drills with the Sakila database#
+
+#1a
+SELECT * FROM actor;
+#1b
+SELECT last_name FROM actor;
+#1c
+SELECT film_id, title, release_year FROM film;
+
+#2a
+SELECT DISTINCT last_name FROM actor;
+#2b
+SELECT DISTINCT postal_code FROM address;
+#2c
+SELECT DISTINCT rating FROM film;
+
+#3a
+SELECT title, description, rating, length FROM film WHERE length > 180;
+#3b
+SELECT payment_id, amount, payment_date FROM payment WHERE payment_date >= '2005-05-27';
+#3c
+SELECT payment_id, amount, payment_date FROM payment WHERE payment_date LIKE '2005-05-27%';
+#3d
+SELECT * FROM customer WHERE last_name LIKE 's%' AND first_name LIKE '%n';
+#3e
+SELECT * FROM customer WHERE active = 0 OR last_name LIKE 'M%';
+#3f
+SELECT * FROM category WHERE category_id > 4 AND name LIKE 'c%' OR name LIKE 's%' OR name LIKE 't%';
+#3g
+SELECT staff_id, first_name, last_name, address_id, picture, email, store_id, active, username, last_update FROM staff WHERE password IS NOT NULL;
+#3h
+SELECT staff_id, first_name, last_name, address_id, picture, email, store_id, active, username, last_update FROM staff WHERE password IS NULL;
+
+#4a
+SELECT phone, district FROM address WHERE district IN ('California', 'England', 'Tiapei', 'West Java');
+#4b - use function - DATE(column name) to look at the date but not the added timestamp
+SELECT payment_id, amount, payment_date FROM payment WHERE DATE(payment_date) IN ('2005-05-25', '2005-05-27', '2005-05-29');
+#4c
+SELECT * FROM film WHERE rating IN ('G', 'PG-13', 'NC-17');
+
+#5a
+SELECT * FROM payment WHERE payment_date BETWEEN '2005-05-25 00:00:00' AND '2005-05-26 23:59:59';
+#5b
+SELECT film_id, title, description FROM film WHERE LENGTH(description) BETWEEN 100 AND 120;
+
+#6a
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 USE employees;
 
